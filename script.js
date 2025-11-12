77d447afdd855c22c57bf08b59485d2ca42088ec
Meciahacks <meciahacks2@svitvasad.ac.in>
2025-11-12 12:45:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-// Simple scroll animation (optional)
-window.addEventListener("scroll", () => {
-  const navbar = document.querySelector(".navbar");
-  if (window.scrollY > 50) {
-    navbar.style.backgroundColor = "rgba(0,0,0,0.8)";
-  } else {
-    navbar.style.backgroundColor = "transparent";
-  }
-});
+
